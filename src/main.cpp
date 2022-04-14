8a3b6357f7bdabb6e426ef39418cc2306ffa457a
Marco <marco.mauricio.244@outlook.com>
2022-04-14 17:05:41 -0500

    Logics done, working on 8x8 Matrix

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,36 @@
 #define DATA 11
 #define CS 12
 #define CLK 13
-
-byte pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+byte pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 LedController lc = LedController(DATA, CLK, CS);
+
+#define LED_GREEN_1 1
+#define LED_YELLOW_1 2
+#define LED_RED_1 4
+#define LED_GREEN_2 8
+#define LED_YELLOW_2 16
+#define LED_RED_2 32
+#define PEAT_1 64
+#define PEAT_2 128
 
 template <size_t N>
 void output(const byte (&pinsArray)[N], const int num, const long delayTime)
 {
-    for (int i = 0; i < N; i++)
+    for (size_t i = 0; i < N; i++)
         digitalWrite(pinsArray[i], (num & (1 << i)) == 0 ? LOW : HIGH);
 
     delay(delayTime);
+}
+
+void TrafficLights(const byte green, const byte yellow, const byte redOther, const byte peatOther)
+{
+    output(pins, green | redOther | peatOther, 1500);
+    for (byte i = 0; i < 3; i++)
+    {
+        output(pins, redOther | peatOther, 250);
+        output(pins, redOther | green | peatOther, 250);
+    }
+    output(pins, redOther | yellow | peatOther, 2000);
 }
 
 void setup()
@@ -24,11 +43,6 @@ void setup()
 
 void loop()
 {
-    for (auto &&item : pins)
-    {
-        digitalWrite(item, HIGH);
-        delay(250);
-        digitalWrite(item, LOW);
-        delay(250);
-    }
+    TrafficLights(LED_GREEN_1, LED_YELLOW_1, LED_RED_2, PEAT_2);
+    TrafficLights(LED_GREEN_2, LED_YELLOW_2, LED_RED_1, PEAT_1);
 }

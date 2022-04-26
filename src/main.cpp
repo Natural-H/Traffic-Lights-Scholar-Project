09477dba50f1895c1cbfd6b8995425ebb43dff20
Marco <marco.mauricio.244@outlook.com>
2022-04-26 12:20:52 -0500

    Refactoring code

--- src/main.cpp
+++ src/main.cpp
@@ -6,50 +6,20 @@ LedControl lc1 = LedControl(10, 9, 8); // DIN1 -> 10 CLK1 -> 9  CS1 -> 8
 byte Stop[] = {0x38, 0x38, 0x10, 0x38, 0x54, 0x10, 0x28, 0x44};
 byte Go[] = {0x18, 0x18, 0x08, 0x3c, 0x4a, 0x18, 0x28, 0x24};
 
-inline void drawImage(LedControl &lc, byte bitmap[8])
-{
+inline void drawImage(LedControl &lc, byte bitmap[8]) {
     for (size_t i = 0; i < 8; i++)
         lc.setColumn(0, i, bitmap[i]);
 }
 
 template <size_t N>
-void output(byte (&pinsArray)[N], byte num, unsigned long delayTime = 0)
-{
+inline void output(byte (&pinsArray)[N], byte num, unsigned long delayTime = 0) {
     for (size_t i = 0; i < N; i++)
         digitalWrite(pinsArray[i], (num & (1 << i)) != 0);
 
     delay(delayTime);
 }
 
-void TrafficLights(byte greenPin, byte yellowPin, byte redPin, LedControl &peat)
-{
-    drawImage(peat, Stop);
-    output(pins, greenPin, 1500);
-    for (byte i = 0; i < 3; i++)
-    {
-        output(pins, 0, 250);
-        output(pins, greenPin, 250);
-    }
-    output(pins, yellowPin, 3000);
-    drawImage(peat, Go);
-    output(pins, redPin);
-    const int pin = 5;
-    for (byte i = 0; i < 4; i++)
-    {
-        tone(pin, 6000, 325);
-        delay(325);
-        tone(pin, 1150, 325);
-        delay(700);
-    }
-    for (byte i = 0; i < 3; i++)
-    {
-        tone(pin, 2500, 150);
-        delay(250);
-    }
-}
-
-void setup()
-{
+void setup() {
     for (auto &&item : pins)
         pinMode(item, OUTPUT);
 
@@ -59,5 +29,23 @@ void setup()
 
 void loop()
 {
-    TrafficLights(1 << 0, 1 << 1, 1 << 2, lc1); // 1 << n is an equivalent to 2^n and pins
+    drawImage(lc1, Stop);
+    output(pins, 1 << 0, 1500);
+    for (byte i = 0; i < 3; i++) {
+        output(pins, 0, 250);
+        output(pins, 1 << 0, 250);
+    }
+    output(pins, 1 << 1, 3000);
+    drawImage(lc1, Go);
+    output(pins, 1 << 2);
+    for (byte i = 0; i < 4; i++) {
+        tone(5, 6000, 325);
+        delay(325);
+        tone(5, 1150, 325);
+        delay(700);
+    }
+    for (byte i = 0; i < 3; i++) {
+        tone(5, 2500, 150);
+        delay(250);
+    }
 }

b7d1d648a8649653548706c874274e6aafb70768
Marco <marco.mauricio.244@outlook.com>
2022-04-26 07:30:45 -0500

    Added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include "LedControl.h"
-byte pins[] = {2, 3, 4, 5, 6, 7}; // G1 -> 2  Y1 -> 3  R1 -> 4 -- G2 -> 5  Y2 -> 6  R2 -> 7
+byte pins[] = {2, 1, 4, 5, 6, 7}; // G1 -> 2  Y1 -> 1  R1 -> 4 -- G2 -> 5  Y2 -> 6  R2 -> 7
 LedControl lc1 = LedControl(10, 9, 8); // DIN1 -> 10 CLK1 -> 9  CS1 -> 8
 LedControl lc2 = LedControl(13, 12, 11); // DIN2 -> 13  CLK2 -> 12  CS2 -> 11
 
@@ -13,7 +13,7 @@ void drawImage(LedControl &lc, byte bitmap[8]) {
 }
 
 template <size_t N>
-void output(byte (&pinsArray)[N], int num, long delayTime) {
+void output(byte (&pinsArray)[N], int num, unsigned long delayTime = 0) {
     for (size_t i = 0; i < N; i++)
         digitalWrite(pinsArray[i], (num & (1 << i)) != 0);
 
@@ -25,10 +25,21 @@ void TrafficLights(byte greenPin, byte yellowPin, byte redOtherPin, LedControl &
     drawImage(peatOther, Go);
     output(pins, greenPin | redOtherPin, 1500);
     for (byte i = 0; i < 3; i++) {
-        output(pins, redOtherPin, 250);
-        output(pins, redOtherPin | greenPin, 250);
+        output(pins, redOtherPin, 500);
+        output(pins, redOtherPin | greenPin, 500);
     }
-    output(pins, redOtherPin | yellowPin, 2000);
+    output(pins, redOtherPin | yellowPin);
+    for (byte i = 0; i < 7; i++) {
+        tone(3, 6000, 200);
+        delay(200);
+        tone(3, 1150, 200);
+        delay(700);
+    }
+    for (byte i = 0; i < 3; i++) {
+        tone(3, 2500, 150);
+        delay(500);
+    }
+    
 }
 
 void setup() {
